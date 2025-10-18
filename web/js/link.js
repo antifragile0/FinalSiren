2b47f4c62f7d197e8833dba332ff671953b52a9e
antifragile0 <133114166+antifragile0@users.noreply.github.com>
2025-10-18 11:58:23 +0700

    Update link.js

--- web/js/link.js
+++ web/js/link.js
@@ -11,13 +11,13 @@ let filteredProxyList = []
 let selectedProxy = null
 const defaultProxyUrl = "https://raw.githubusercontent.com/AFRcloud/ProxyList/refs/heads/main/ProxyList.txt"
 
-const serverDomains = ["afrcloud.biz.id"]
+const serverDomains = ["siren.tedx.dpdns.org"]
 let selectedServerDomain = serverDomains[0] // Default to first domain
 const defaultUUID = "bbbbbbbb-cccc-4ddd-eeee-ffffffffffff"
 const itemsPerPage = 10
 let currentPage = 1
 
-const pathTemplate = "/afrcloud/{ip}-{port}"
+const pathTemplate = "/tedx/{ip}-{port}"
 
 // Array of bug options for easy management
 const bugOptions = [
